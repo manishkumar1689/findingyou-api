0954d8eb4c316c969017cf5f6425a89258b5d204
Neil Gardner <neilg@qpc-H97-D3H>
2020-03-18 13:11:25 +0000

    coverted most JS files to TS, need to create more interfaces and classes before ready for switch

---
+++ src/.config.default.ts
@@ -0,0 +1,24 @@
+/*
+ Copy this file to .config.js in the route directory
+*/
+
+export const mongo = {
+  name: 'findingyou',
+  user: 'stargazer',
+  pass: 'T0XcJfpIBsFGkUY7',
+};
+
+export const redisOptions = {
+  store: redisStore,
+  host: 'localhost',
+  port: 6379,
+};
+
+export const ephemerisPath: '/usr/share/libswe/ephe';
+
+export const ephemerisDefaults: {
+  altitude: 30,
+  pressure: 1000,
+  temperature: 10,
+  sid_mode: 'SE_SIDM_TRUE_CITRA'
+}
