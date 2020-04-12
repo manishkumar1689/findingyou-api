c19b12af8c0a4835abab36278911000790810fa8
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2020-04-12 21:16:47 +0100

    added outline for new chart schema + saving user data

--- src/user/interfaces/payment-option.interface.ts
+++ src/user/interfaces/payment-option.interface.ts
@@ -6,6 +6,7 @@ export interface PaymentOption {
   readonly key: string;
   readonly name: string;
   readonly amount: number;
+  readonly curr: string;
   readonly period: string;
   readonly duration: number;
   readonly maxRepeats: number;
