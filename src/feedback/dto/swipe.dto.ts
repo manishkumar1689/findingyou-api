8c002733cb884453db2f3a1be278c6a05989fe7b
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2021-09-17 16:27:53 +0100

    More work on teh five birds

---
+++ src/feedback/dto/swipe.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SwipeDTO {
+  @ApiProperty()
+  readonly from: string;
+
+  @ApiProperty()
+  readonly to: string;
+
+  @ApiProperty()
+  readonly value: number;
+}
